aaf65463e71c460ee1d647bee04487b7f17429e9
snltd <github@id264.net>
2026-08-07 18:25:30 +0100

    allow automatic require of .fnl

--- main.lua
+++ main.lua
@@ -1,13 +1,14 @@
 love.filesystem.setRequirePath("?.lua;.lib/?.lua;.lib/?/init.lua")
 fennel = require("fennel")
 debug.traceback = fennel.traceback
-table.insert(package.loaders, function(filename)
-   if love.filesystem.getInfo(filename) then
-      return function(...)
-         return fennel.eval(love.filesystem.read(filename), {env=_G, filename=filename}, ...), filename
-      end
-   end
+
+table.insert(package.loaders, function(modname)
+  local filename = modname:gsub("%.", "/") .. ".fnl"
+  if love.filesystem.getInfo(filename) then
+    return function(...)
+      return fennel.eval(love.filesystem.read(filename), { env = _G, filename = filename }, ...), filename
+    end
+  end
 end)
--- jump into Fennel
-require("main.fnl")
-require("fennel").eval(love.filesystem.read("main.fnl"), {env=_G})
+
+require("quasi-tron")
